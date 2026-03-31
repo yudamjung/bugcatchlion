0771b9fff07efcf3917b1eedeecf8ebbd7d3e000
Yudam Jung <udamee1025@gmail.com>
2026-03-31 11:57:26 +0900

    fix: modal_body + shadow 결합

--- shadow.js
+++ shadow.js
@@ -1,8 +1,8 @@
 // HTML 문서가 다 읽힌 뒤에 실행되도록 설정
 document.addEventListener('DOMContentLoaded', () => {
-    
+
     // 'profile-card'라는 클래스를 가진 모든 요소를 가져옴
-    const cards = document.querySelectorAll('.profile-card');
+    const cards = document.querySelectorAll('.modal_body');
 
     // 3. 찾아낸 카드들을 하나씩 꺼내어 각각에게 이벤트리스너 추가
     //이벤트리스너란 특정 이벤트가 발생 했을 때 실행 되는 함수
